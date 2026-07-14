7b2043be3f9e179c157aafe810b2fb2f0f28119c
Esha Lokhande <eshalokhande04@gmail.com>
2026-07-14 18:39:38 +0530

    Added responsive Navbar with mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,42 @@ import { useState } from "react";
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <nav>
-            <Link href="/">Esha.dev</Link>
+        <nav className="sticky top-0 z-50 bg-white/80 backdrop-blur-md shadow-md">
 
-            <div>
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/skills">Skills</Link>
-                <Link href="/projects">Projects</Link>
-                <Link href="/contact">Contact</Link>
+            <div className="flex items-center justify-between max-w-5xl mx-auto px-6 py-4 ">
+                <Link href="/" className="text-xl text-purple-600 font-bold">Esha
+                    <span className ="text-gray-800">.dev</span>
+                </Link>
+
+                <div className=" hidden sm:flex gap-8">
+                    <Link href="/" className="font-medium text-gray-700 transition hover:text-purple-600">Home</Link>
+
+                    <Link href="/about" className="font-medium text-gray-700 transition hover:text-purple-600">About</Link>
+
+                    <Link href="/skills" className="font-medium text-gray-700 transition hover:text-purple-600">Skills</Link>
+
+                    <Link href="/projects" className="font-medium text-gray-700 transition hover:text-purple-600">Projects</Link>
+
+                    <Link href="/contact" className="font-medium text-gray-700 transition hover:text-purple-600">Contact</Link>
+
+                </div>
+
+                <button className="sm:hidden text-2xl"
+                    onClick={ ()=> setIsOpen(!isOpen)}>
+                    {isOpen ? 'X' : '☰'}
+                </button>
             </div>
+
+            {isOpen && (
+                <div className="flex flex-col gap-4 px-6 pb-4 sm:hidden">
+                <Link href="/" onClick={() => setIsOpen(false)} className="font-medium text-gray-700">Home</Link>
+                <Link href="/about" onClick={() => setIsOpen(false)} className="font-medium text-gray-700">About</Link>
+                <Link href="/skills" onClick={() => setIsOpen(false)} className="font-medium text-gray-700">Skills</Link>
+                <Link href="/projects" onClick={() => setIsOpen(false)} className="font-medium text-gray-700">Projects</Link>
+                <Link href="/contact" onClick={() => setIsOpen(false)} className="font-medium text-gray-700">Contact</Link>
+                </div>
+            )}
+
         </nav>
     )
 }
